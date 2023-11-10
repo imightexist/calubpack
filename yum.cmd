92605ffa6fc3d2691de6d8507e248425fdce46f5
imightexist <94249457+imightexist@users.noreply.github.com>
2023-11-09 18:44:11 -0600

    Update yum.cmd

--- yum.cmd
+++ yum.cmd
@@ -1,18 +1,21 @@
+set glupdate=1
 net session
 if %errorlevel% == 1 exit
 
-aria2c -x16 -s16 -m16 https://archive.org/download/7zcmd/7z.dll
-aria2c -x16 -s16 -m16 https://archive.org/download/7zcmd/7z.exe
-aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/glupdate2.7z/comp/gl32.dll
-aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/glupdate2.7z/comp/gl64.dll
 aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/calubsniffer.7z
 aria2c https://raw.githubusercontent.com/imightexist/calubpack/main/fix.cmd
+
+if %glupdate% == 0 goto yip
+aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/glupdate2.7z/comp/gl32.dll
+aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/glupdate2.7z/comp/gl64.dll
 takeown /f C:\windows\system32\opengl32.dll
 takeown /f C:\windows\syswow64\opengl32.dll
 icacls C:\windows\system32\opengl32.dll /grant Administrators:F
 icacls C:\windows\syswow64\opengl32.dll /grant Administrators:F
 copy /y gl32.dll C:\windows\syswow64\opengl32.dll
 copy /y gl64.dll C:\windows\system32\opengl32.dll
+
+:yip
 7z x calubsniffer.7z -o"%userprofile%\Desktop\minecraft"
 copy /y fix.cmd "%userprofile%\Desktop\minecraft\! run.bat"
 del "%userprofile%\Desktop\minecraft\! install.cmd"
