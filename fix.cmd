18eba94d8b50ec349eaaa618fdb353401c533de1
imightexist <94249457+imightexist@users.noreply.github.com>
2023-11-09 18:32:05 -0600

    Update fix.cmd

--- fix.cmd
+++ fix.cmd
@@ -1,7 +1,6 @@
 set user=notch
 
-echo you can change ur usernam!!! > advertisement.txt
-echo you should join dis server NOW! 147.185.221.180:36427 >> advertisement.txt
+echo you should join dis server NOW! 147.185.221.180:36427 > advertisement.txt
 start advertisement.txt
 
 "j/bin/java" -cp "*" -Djava.library.path="natives" -Dorg.lwjgl.librarypath="natives" -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump net.minecraft.client.main.Main --server 147.185.221.180 --port 36427 --username %user% --version 1.20.1 --gameDir "%cd%" --assetsDir assets --assetIndex 8 --accessToken %user%
