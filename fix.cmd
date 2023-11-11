dfd836dbf7fe4cbc4949458991802f9802de8d3f
imightexist <94249457+imightexist@users.noreply.github.com>
2023-11-11 16:31:30 -0600

    Update fix.cmd

--- fix.cmd
+++ fix.cmd
@@ -1,6 +1,6 @@
 set user=notch
 
-rem echo you should join dis server NOW! 147.185.221.180:36427 > advertisement.txt
+rem echo you should join dis server NOW! 147.185.221.180:36427
 echo you should join dis server NOW! dgx.ignorelist.com > advertisement.txt
 echo join the dickor: discord.gg/4npKFF3vMP >> advertisement.txt
 start advertisement.txt
