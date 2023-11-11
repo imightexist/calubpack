3d4caf47884cb009ccdd3ac654056ea599e77494
imightexist <94249457+imightexist@users.noreply.github.com>
2023-11-11 15:43:31 -0600

    Update yum.cmd

--- yum.cmd
+++ yum.cmd
@@ -1,14 +1,14 @@
 pushd %~dp0
 set glupdate=1
 
-aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/calubsniffer.7z
+aria2c -x16 -s16 -m16 https://github.com/imightexist/calubpack/releases/download/moment/calubsniffer.7z
 aria2c https://raw.githubusercontent.com/imightexist/calubpack/main/fix.cmd
 
 if %glupdate% == 0 goto yip
 net session
 if %errorlevel% == 1 exit
 rem aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/glupdate2.7z/comp/gl32.dll
-aria2c -x16 -s16 -m16 https://archive.org/download/calubcraft/glupdate2.7z/comp/gl64.dll
+aria2c -x16 -s16 -m16 https://github.com/imightexist/calubpack/releases/download/moment/gl64.dll
 takeown /f C:\windows\system32\opengl32.dll
 rem takeown /f C:\windows\syswow64\opengl32.dll
 icacls C:\windows\system32\opengl32.dll /grant Administrators:F
