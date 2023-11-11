aa406c59a1eb38b915039dffe4f16ee0aa9359b8
imightexist <94249457+imightexist@users.noreply.github.com>
2023-11-11 14:01:57 -0600

    Update fix.cmd

--- fix.cmd
+++ fix.cmd
@@ -1,6 +1,7 @@
 set user=notch
 
 echo you should join dis server NOW! 147.185.221.180:36427 > advertisement.txt
+echo join the dickor: discord.gg/4npKFF3vMP >> advertisement.txt
 start advertisement.txt
 
 "j/bin/java" -cp "*" -Djava.library.path="natives" -Dorg.lwjgl.librarypath="natives" -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump net.minecraft.client.main.Main --server 147.185.221.180 --port 36427 --username %user% --version 1.20.1 --gameDir "%cd%" --assetsDir assets --assetIndex 8 --accessToken %user%
